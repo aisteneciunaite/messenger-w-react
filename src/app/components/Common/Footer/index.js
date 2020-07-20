8c8614148d5008528cf990800be3ecb1f7acd0f8
Aiste Neciunaite <a.neciunaite@gmail.com>
2020-07-20 21:49:36 +0300

    new icons

--- src/app/components/Common/Footer/index.js
+++ src/app/components/Common/Footer/index.js
@@ -4,17 +4,7 @@ import './index.scss';
 function Footer() {
   return (
     <footer>
-      <span>
-        Icons made by{' '}
-        <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">
-          Pixel perfect
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          {' '}
-          www.flaticon.com
-        </a>
-      </span>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </footer>
   );
 }

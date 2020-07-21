b3e037d07b541e25f3d8ffcd0dfd5e83d919c277
Aiste Neciunaite <a.neciunaite@gmail.com>
2020-07-21 09:32:53 +0300

    modal component

--- src/app/components/Common/Modal/index.js
+++ src/app/components/Common/Modal/index.js
@@ -1,18 +1,34 @@
 import React, { useRef } from 'react';
 import './index.scss';
 
-function Modal({ setShowModal, children }) {
-    const modal = useRef(null);
-    function hideModal(e) {
-        if (e.target !== modal) {
-            setShowModal(false);
-        }
+// import Button from '../Button';
+import Title from '../Title';
+import Button from '../Button';
+
+function Modal({ setShowModal, header, children }) {
+  const dialog = useRef(null);
+  function hideModal(e) {
+    if (!dialog.current.contains(e.target)) {
+      setShowModal(false);
     }
-    return (
-        <div className="Modal" onClick={hideModal}>
-            {children}
+  }
+  return (
+    <div className="Modal" onClick={hideModal}>
+      <div className="Modal__dialog" ref={dialog}>
+        <div className="Modal__content">
+          <div className="Modal__header">
+            <Title level="5" className="Modal__title">
+              {header}
+            </Title>
+            <Button className="close" onClick={() => setShowModal(false)}>
+              <span aria-hidden="true">&times;</span>
+            </Button>
+          </div>
+          {children}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Modal;

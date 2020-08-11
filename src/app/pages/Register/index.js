88d7b7adbcccc761628fbc4cf384da8039ea28f2
Aiste Neciunaite <a.neciunaite@gmail.com>
2020-08-11 17:10:16 +0300

    additional input validations

--- src/app/pages/Register/index.js
+++ src/app/pages/Register/index.js
@@ -9,6 +9,37 @@ import auth from 'authentication';
 import Form from 'app/components/Common/Form';
 import Input from 'app/components/Common/Input';
 
+const passwordValidator = {
+  digit: {
+    expression: new RegExp('(?=.*\\d)'),
+    errorMessage: 'Slaptažodis turi turėti skaičių',
+  },
+  uppercase: {
+    expression: new RegExp('(?=.*[A-Z])'),
+    errorMessage: 'Slaptažodis turi turėti didžiąją raidę',
+  },
+  lowercase: {
+    expression: new RegExp('(?=.*[A-Z])'),
+    errorMessage: 'Slaptažodis turi turėti didžiąją raidę',
+  },
+  length: {
+    expression: new RegExp('.{8,}'),
+    errorMessage: 'Slaptažodis turi būti bent 8 simbolių',
+  },
+};
+
+const emailValidator = {
+  expression: new RegExp(
+    '^([A-Z|a-z|0-9](\\.|_){0,1})+[A-Z|a-z|0-9]\\@([A-Z|a-z|0-9])+((\\.){0,1}[A-Z|a-z|0-9]){2}\\.[a-z]{2,3}$'
+  ),
+  errorMessage: 'Netinkamas adresas',
+};
+
+const usernameValidator = {
+  expression: new RegExp('^([A-Za-z0-9]){4,20}$'),
+  errorMessage: 'Netinkamas vartotojo vardas',
+};
+
 function Register() {
   const history = useHistory();
   const authenticated = useSelector(state => !!auth.selectors.getToken(state));
@@ -65,12 +96,26 @@ function Register() {
       passwordRepeat: passwordRepeatInput.current.value,
     };
 
-    if (passwordRepeatInput.current.value !== passwordInput.current.value) {
+    if (!usernameValidator.expression.test(credentials.username)) {
+      setUsernameError(usernameValidator.errorMessage);
+      console.log('bad username');
+    } else if (!emailValidator.expression.test(credentials.email)) {
+      setEmailError(emailValidator.errorMessage);
+    } else if (!passwordValidator.length.expression.test(credentials.password)) {
+      setPasswordError(passwordValidator.length.errorMessage);
+    } else if (!passwordValidator.uppercase.expression.test(credentials.password)) {
+      setPasswordError(passwordValidator.uppercase.errorMessage);
+    } else if (!passwordValidator.lowercase.expression.test(credentials.password)) {
+      setPasswordError(passwordValidator.lowercase.errorMessage);
+    } else if (!passwordValidator.digit.expression.test(credentials.password)) {
+      setPasswordError(passwordValidator.digit.errorMessage);
+    } else if (passwordRepeatInput.current.value !== passwordInput.current.value) {
       setPasswordError('Nesutampa slaptazodziai');
       setPasswordRepeatError('Nesutampa slaptazodziai');
     } else {
       dispatch(auth.actions.register(credentials));
     }
+    // dispatch(auth.actions.register(credentials)); // for testing without validation
   }
 
   useEffect(() => {
